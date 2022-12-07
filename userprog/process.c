14b9a738189c7f4a2d86fddcaee09f73f1e03fe3
dlawldn10 <dlawldn10@gmail.com>
2022-12-07 22:10:21 +0900

    git push test

--- userprog/process.c
+++ userprog/process.c
@@ -863,6 +863,7 @@ setup_stack (struct intr_frame *if_) {
 	// success = vm_alloc_page(VM_ANON | VM_MARKER_0, stack_bottom, true) && vm_claim_page(stack_bottom);
 	// if (success)
 	// 	if_->rsp = USER_STACK;
+	//
 
 	if (vm_alloc_page(VM_ANON | VM_MARKER_0, stack_bottom, true)) {
 		success = vm_claim_page(stack_bottom);
