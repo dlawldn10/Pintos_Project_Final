57b19611c98ad17e8e00907eaf7123952ce1999c
Haydn389 <ekchoi0502@gmail.com>
2022-12-09 05:04:10 +0000

    chore : munmap 주석추가

--- vm/file.c
+++ vm/file.c
@@ -53,13 +53,17 @@ file_backed_destroy (struct page *page) {
 }
 
 /* Do the mmap */
+/* addr부터 시작하는 연속된 유저 가상 메모리 공간에 page들을 만들어 
+file의 offset부터 length에 해당하는 file의 정보를 각 page마다 저장한다.
+나중에 프로세스가 이 page에 접근해서 page fault를 발생시키면 
+physical frame과 mapping하여 (claim) disk에서 file data를 frame에 복사함*/
 void *
 do_mmap (void *addr, size_t length, int writable,
 		struct file *file, off_t offset) {
 
 	size_t read_bytes = length < file_length(file) ? length : file_length(file);
 	size_t zero_bytes = PGSIZE - read_bytes % PGSIZE;
-	/*file을 reopen해야 할까?
+	/*file reopen해야 할까?
 	testcase: mmap-close 처리를 위해 필요!!!
 	mmap이 lazy load 방식으로 구현되었기 때문에, mmap이 lazy하게 load되기 전에 
 	file이 close되었을 경우 file을 load하지 못하는 상황이 생김
@@ -95,19 +99,22 @@ do_mmap (void *addr, size_t length, int writable,
 /* Do the munmap */
 void
 do_munmap (void *addr) {
+	/* TODO: Destroy all the supplemental_page_table hold by thread and
+     * TODO: writeback all the modified contents to the storage. */
 	struct list *m_list = &thread_current()->mmap_list;
 	struct list_elem *e = list_begin(m_list);
 	while (e != list_tail(m_list))
 	{
 		struct page *page = spt_find_page(&thread_current()->spt, addr);
 		struct file_page *fp = list_entry(e,struct file_page, file_elem);
-
+		/* 수정된 페이지(dirty bit==1)라면, 변경 사항을 파일(disk)에 다시 기록한다 */
 		if(pml4_is_dirty(thread_current()->pml4, page->va)){
-			/* file의 offset 업데이트*/
+			/*buffer의내용을 file의 file_ofs부터 size만큼기록*/
 			file_write_at(fp->file,addr,fp->page_read_byte, fp->ofs);
+			 /* dirty bit을 0으로 만든다. */
 			pml4_set_dirty(thread_current()->pml4, page->va, false);
 		}
-
+		/* present bit을 0으로 만든다. */
 		pml4_clear_page(thread_current()->pml4, page->va);
 		addr += PGSIZE;
 		e = list_next(e);
